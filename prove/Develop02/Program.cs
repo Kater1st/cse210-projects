a66d51d30df26d42a50c0323a6252c35dfba3468
Kater1st <kat24007@byui.edu>
2024-10-03 15:15:51 +0200

    final journal prog

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,131 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Entry
+{
+    public string Date { get; }
+    public string Prompt { get; }
+    public string Response { get; }
+
+    public Entry(string date, string prompt, string response)
+    {
+        Date = date;
+        Prompt = prompt;
+        Response = response;
+    }
+
+    public override string ToString()
+    {
+        return $"{Date} | {Prompt} | {Response}";
+    }
+}
+
+class Journal
+{
+    public List<Entry> Entries { get; private set; } = new List<Entry>();
+
+    public void AddEntry(Entry entry)
+    {
+        Entries.Add(entry);
+    }
+
+    public void DisplayEntries()
+    {
+        foreach (var entry in Entries)
+        {
+            Console.WriteLine(entry);
+        }
+    }
+
+    public void SaveToFile(string filename)
+    {
+        using (StreamWriter writer = new StreamWriter(filename))
+        {
+            foreach (var entry in Entries)
+            {
+                writer.WriteLine($"{entry.Date}|{entry.Prompt}|{entry.Response}");
+            }
+        }
+    }
+
+    public void LoadFromFile(string filename)
+    {
+        Entries.Clear();
+        foreach (var line in File.ReadAllLines(filename))
+        {
+            var parts = line.Split('|');
+            if (parts.Length == 3)
+            {
+                Entries.Add(new Entry(parts[0], parts[1], parts[2]));
+            }
+        }
+    }
+}
 
 class Program
 {
+    static List<string> prompts = new List<string>
+    {
+        "Who was the most interesting person I interacted with today?",
+        "What was the best part of my day?",
+        "How did I see the hand of the Lord in my life today?",
+        "What was the strongest emotion I felt today?",
+        "If I had one thing I could do over today, what would it be?"
+    };
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Journal journal = new Journal();
+        string choice;
+        do
+        {
+            Console.WriteLine("1. Write a new entry");
+            Console.WriteLine("2. Display journal");
+            Console.WriteLine("3. Save journal to file");
+            Console.WriteLine("4. Load journal from file");
+            Console.WriteLine("5. Exit");
+            choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    WriteNewEntry(journal);
+                    break;
+                case "2":
+                    journal.DisplayEntries();
+                    break;
+                case "3":
+                    SaveJournalToFile(journal);
+                    break;
+                case "4":
+                    LoadJournalFromFile(journal);
+                    break;
+            }
+        } while (choice != "5");
+    }
+
+    static void WriteNewEntry(Journal journal)
+    {
+        Random rand = new Random();
+        string prompt = prompts[rand.Next(prompts.Count)];
+        Console.WriteLine(prompt);
+        string response = Console.ReadLine();
+        string date = DateTime.Now.ToShortDateString();
+        journal.AddEntry(new Entry(date, prompt, response));
+    }
+
+    static void SaveJournalToFile(Journal journal)
+    {
+        Console.Write("Enter filename to save: ");
+        string filename = Console.ReadLine();
+        journal.SaveToFile(filename);
+    }
+
+    static void LoadJournalFromFile(Journal journal)
+    {
+        Console.Write("Enter filename to load: ");
+        string filename = Console.ReadLine();
+        journal.LoadFromFile(filename);
     }
 }
